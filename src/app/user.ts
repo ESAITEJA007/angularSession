6d13fa50a6c8ac858f48d744fbc6147c973569c3
saiteja <enimidigandlasaiteja@gmail.com>
2023-06-06 09:53:20 -0500

    Added constructor

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,13 @@
 export class User {
-    Name: string = '';
-    Email: string = '';
-    Password: string = '' ;
-    DoB: Date = new Date();
+  Name: string = '';
+  Email: string = '';
+  Password: string = '';
+  DoB: Date = new Date();
+
+  constructor(name : string, email: string, pass: string, doB: Date) {
+   this.Name = name;
+   this.Email = email;
+   this.Password = pass;
+   this.DoB = doB;
+  }
 }
